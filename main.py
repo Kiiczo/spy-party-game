9d32f4640d36ab56eb0d0feb18dc566b799cee2e
Kiiczo <witold.zzak@gmail.com>
2025-07-21 13:57:28 +0000

    Refactor code

--- main.py
+++ main.py
@@ -11,33 +11,28 @@ showing = False
 reseting = False
 
 
+next_button = document.querySelector("#next-button")
+submit_button = document.querySelector("#submit")
+locations_text = document.querySelector("#locations")
+players_text = document.querySelector("#players")
+game_text = document.querySelector("#game-text")
+
 def game():
     global spy, actual, here
-    output_div = document.querySelector("#output")
-    output_div.innerHTML = ""
+    game_text.style.display = "block"
+    next_button.innerText = "Next"
 
     spy = random.choice(players)
     actual = 0
     while here == None or here == "":
         here = random.choice(locations)
 
-    game_text = document.createElement("div")
-    game_text.id = "game-text"
     game_text.innerText = "Game is starting! Click next to continue"
-    output_div.appendChild(game_text)
 
-    next_button = document.createElement("button")
-    next_button.innerText = "Next"
-    next_button.id = "next-button"
-    next_button.addEventListener("click", create_proxy(next_step))
-
-    output_div.appendChild(document.createElement("br"))
-    output_div.appendChild(next_button)
+    next_button.style.display = "block"
 
 def next_step(event):
     global actual, showing, reseting,game_text,next_button
-    game_text = document.querySelector("#game-text")
-    next_button = document.querySelector("#next-button")
     
     if not showing and not reseting:
         game_text.innerText = players[actual] + " are you holding the phone?"
@@ -60,11 +55,12 @@ def next_step(event):
 
 def reset():
     global locations,players,spy,actual,here,showing,reseting
+    
     submit_button.style.display = "block"
     locations_text.style.display = "block"
     players_text.style.display = "block"
-    game_text.remove()
-    next_button.remove()
+    game_text.style.display = "none"
+    next_button.style.display = "none"
     locations = None
     players = None
     spy = None
@@ -76,14 +72,9 @@ def reset():
 def submit_event(event):
     global locations, players, submit_button,players_text,locations_text
     
-    locations_text = document.querySelector("#locations")
     locations = locations_text.value.strip().splitlines()
-
-    
-    players_text = document.querySelector("#players")
     players = players_text.value.split()
 
-    submit_button = document.querySelector("#submit")
     submit_button.style.display = "none"
     locations_text.style.display = "none"
     players_text.style.display = "none"
